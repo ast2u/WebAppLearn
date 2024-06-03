81417ed34b313d17e82fbf769573e30c5648df31
Carlo Astudillo <carloasto11@gmail.com>
2024-06-03 22:29:18 +0800

    Add project files.

---
+++ WebAppLearn/Pages/People.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppLearn.Pages
+{
+    public class PeopleModel : PageModel
+    {
+        public void OnGet()
+
+        {
+        }
+    }
+}
